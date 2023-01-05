97e82a4b4aeac167a669dd1b709cd4fc8e2b2b2c
Naufalspurnomo <114010162+Naufalspurnomo@users.noreply.github.com>
2023-01-05 11:14:07 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author shidq
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Game frame = new Game();
+        frame.setVisible(true);
+        MainMenu menu = new MainMenu();
+        menu.setVisible(true);
+
+    }
+    
+}
